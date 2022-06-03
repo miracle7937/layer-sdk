7106d659a06e377b636af3df9aa88d69cc46432e
Jose Esteve Tendero <jose.tendero@ubanquity.com>
2022-06-03 15:22:37 +0200

    MR fixes

--- lib/domain_layer/use_cases/cashback_history/load_casback_history_use_case.dart
+++ lib/domain_layer/use_cases/cashback_history/load_casback_history_use_case.dart
@@ -11,6 +11,9 @@ class LoadCashbackHistoryUseCase {
   }) : _repository = repository;
 
   /// Returns cashback history.
+  ///
+  /// Use the [from] and [to] values to retrieve the cashback history for
+  /// a specific date range.
   Future<CashbackHistory> call({
     DateTime? from,
     DateTime? to,
--- lib/domain_layer/use_cases/offers/load_offer_by_id_use_case.dart
+++ lib/domain_layer/use_cases/offers/load_offer_by_id_use_case.dart
@@ -11,6 +11,13 @@ class LoadOfferByIdUseCase {
   }) : _repository = repository;
 
   /// Returns the offer correspoding to the passed id.
+  ///
+  /// The [offerId] is required and corresponds to the offer id that you want
+  /// to retrieve.
+  ///
+  /// The [latitude] and [longitude] values are optional and represents the
+  /// current location. If indicated, the offer will be returned with the
+  /// distance field filled.
   Future<Offer> call({
     required int offerId,
     bool forceRefresh = false,
