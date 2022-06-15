370be20909b8ee153a5c5d430f4457db0ab2c6f8
Jose Esteve Tendero <jose.tendero@ubanquity.com>
2022-06-15 13:35:45 +0200

    RM-39873 | Removed the geofencing package and related files from the layer sdk

--- lib/presentation_layer/app/bank_app.dart
+++ lib/presentation_layer/app/bank_app.dart
@@ -254,9 +254,6 @@ class BankAppState extends State<BankApp> {
           ),
         ),
       ),
-      BlocProvider<GeofencingCubit>(
-        create: (_) => GeofencingCubit(),
-      ),
     ];
   }
 
