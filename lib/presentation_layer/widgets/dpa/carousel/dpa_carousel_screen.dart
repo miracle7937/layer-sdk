15000707585ddb537caadd5b4dd883f292cd1580
Jose Esteve Tendero <jose.tendero@ubanquity.com>
2022-07-19 16:51:41 +0200

    removed unused inport

--- lib/presentation_layer/widgets/dpa/carousel/dpa_carousel_screen.dart
+++ lib/presentation_layer/widgets/dpa/carousel/dpa_carousel_screen.dart
@@ -4,7 +4,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../data_layer/environment.dart';
 import '../../../../features/dpa.dart';
-import '../../../extensions.dart';
 import '../../../resources.dart';
 import '../../../widgets.dart';
 
