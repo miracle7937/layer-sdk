cb500fdb6d36703a34dd5803855ffcb20efbf247
marwa.karaki <marwa.karaki@wearelayer.com>
2022-07-28 15:33:55 +0300

    post jumio loader

--- lib/presentation_layer/widgets/dpa/fullscreen_loader/dpa_fullscreen_loader.dart
+++ lib/presentation_layer/widgets/dpa/fullscreen_loader/dpa_fullscreen_loader.dart
@@ -1,25 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import '../../../../layer_sdk.dart';
 
 /// Creates the fullscreen loader for the dpa flow.
 class DPAFullscreenLoader extends StatelessWidget {
   /// Creates a new [DPAFullscreenLoader].
+
+  final String? asset;
+
+  /// constuctor of [DPAFullscreenLoader]
   const DPAFullscreenLoader({
     Key? key,
+    this.asset,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final design = DesignSystem.of(context);
 
-    return Container(
-      // TODO: Add this color to the design system (modal/overlay)
-      color: Color(0xFF191A19).withOpacity(0.64),
-      child: Center(
-        child: DKLoader(
-          size: 72,
-          startColor: design.basePrimaryWhite,
-          endColor: design.basePrimaryWhite.withOpacity(0),
+    final translation = Translation.of(context);
+    return Scaffold(
+      body: Container(
+        color: DesignSystem.of(context).surfaceOctonary1,
+        child: SafeArea(
+          child: Stack(
+            children: [
+              if (asset != null)
+                Positioned(
+                    top: 81.0,
+                    left: (MediaQuery.of(context).size.width / 2) - 60.5,
+                    right: (MediaQuery.of(context).size.width / 2) - 60.5,
+                    child: SvgPicture.asset(asset!)),
+              Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    DKLoader(),
+                    SizedBox(height: 42.0),
+                    Text(
+                      translation.translate('please_wait'),
+                    ),
+                    SizedBox(height: 10.0),
+                    Text(
+                      translation
+                          .translate('we_are_verifying_your_information'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
