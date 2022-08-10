5554c0ad05233a184db1b7f58458e2df688123e5
Victor Ferreira <victor.ferreira@wearelayer.com>
2022-08-10 07:15:33 +0000

    RM-41014 - Account top up receipt implementation

--- lib/domain_layer/models.dart
+++ lib/domain_layer/models.dart
@@ -4,6 +4,7 @@ export 'models/account/account.dart';
 export 'models/account/account_filter.dart';
 export 'models/account/account_info.dart';
 export 'models/account/account_preferences.dart';
+export 'models/account/account_top_up_request.dart';
 export 'models/account_loan/account_loan.dart';
 export 'models/account_loan/account_loan_payment.dart';
 export 'models/account_transaction/account_transaction.dart';
