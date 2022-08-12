04635cfdf46cbc5ea65884ac3d88ab0bbbdbb5be
marwa.karaki <marwa.karaki@wearelayer.com>
2022-08-12 09:55:51 +0300

    remove jumio loader from otp screen

--- lib/presentation_layer/widgets/dpa/fullscreen_loader/dpa_fullscreen_loader.dart
+++ lib/presentation_layer/widgets/dpa/fullscreen_loader/dpa_fullscreen_loader.dart
@@ -16,39 +16,48 @@ class DPAFullscreenLoader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final translation = Translation.of(context);
-    return Scaffold(
-      body: Container(
-        color: DesignSystem.of(context).surfaceOctonary1,
-        child: SafeArea(
-          child: Stack(
-            children: [
-              if (asset != null)
-                Positioned(
-                    top: 81.0,
-                    left: (MediaQuery.of(context).size.width / 2) - 60.5,
-                    right: (MediaQuery.of(context).size.width / 2) - 60.5,
-                    child: SvgPicture.asset(asset!)),
-              Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
+    final design = DesignSystem.of(context);
+    return asset != null
+        ? Scaffold(
+            body: Container(
+              color: DesignSystem.of(context).surfaceOctonary1,
+              child: SafeArea(
+                child: Stack(
                   children: [
-                    DKLoader(),
-                    SizedBox(height: 42.0),
-                    Text(
-                      translation.translate('please_wait'),
-                    ),
-                    SizedBox(height: 10.0),
-                    Text(
-                      translation
-                          .translate('we_are_verifying_your_information'),
+                    Positioned(
+                        top: 81.0,
+                        left: (MediaQuery.of(context).size.width / 2) - 60.5,
+                        right: (MediaQuery.of(context).size.width / 2) - 60.5,
+                        child: SvgPicture.asset(asset!)),
+                    Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          DKLoader(),
+                          SizedBox(height: 42.0),
+                          Text(
+                            translation.translate('please_wait'),
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            translation
+                                .translate('we_are_verifying_your_information'),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          )
+        : Container(
+            // TODO: Add this color to the design system (modal/overlay)
+            color: Color(0xFF191A19).withOpacity(0.64),
+            child: Center(
+                child: DKLoader(
+                    size: 72,
+                    startColor: design.basePrimaryWhite,
+                    endColor: design.basePrimaryWhite.withOpacity(0))));
   }
 }
