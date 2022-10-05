4ce25ce2b755aef3d7d7a1844743b8c68ab6b998
Gizem Cetin <gizem.cetin@wearelayer.com>
2022-10-05 13:36:36 +0300

    view status added

--- lib/presentation_layer/resources.dart
+++ lib/presentation_layer/resources.dart
@@ -3,3 +3,4 @@ library resources;
 export 'resources/app_theme.dart';
 export 'resources/fl_images.dart';
 export 'resources/storage_keys.dart';
+export 'resources/view_status.dart';
---
+++ lib/presentation_layer/resources/view_status.dart
@@ -0,0 +1,14 @@
+/// The view status
+enum ViewStatus{
+   ///The loading status to show loader
+  loading,
+
+  ///The error status to error card view
+  error,
+
+  ///The idle status to show builder
+  idle,
+
+  ///This status for the empty data
+  noData
+}
