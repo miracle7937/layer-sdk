96f2a0545a0043e1738d9812c542630c91a967f6
marwa karaki <marwa.karaki@wearelayer.com>
2022-12-15 15:39:13 +0200

    fix pipeline

--- lib/_migration/flutter_layer/src/widgets/text_fields/auto_padding_keyboard_view.dart
+++ lib/_migration/flutter_layer/src/widgets/text_fields/auto_padding_keyboard_view.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 
 import '../../../../../presentation_layer/resources.dart';
-import 'input_done_view.dart';
 
 /// View that will add/remove a padding to the child view when
 /// the keyboard is shown/hidden
