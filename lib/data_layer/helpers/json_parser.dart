974e02109103267aa9b14f71f3cc7ecfee9202b7
Paulo Matias <paulo.matias@wearelayer.com>
2023-02-28 10:27:49 -0300

    fixed date parse

--- lib/data_layer/helpers/json_parser.dart
+++ lib/data_layer/helpers/json_parser.dart
@@ -13,8 +13,16 @@ class JsonParser {
 
   /// Parses a value into a [DateTime].
   static DateTime? parseDate(dynamic value) {
-    if (value == null || value is! int) return null;
-    return DateTime.fromMillisecondsSinceEpoch(value);
+    try {
+      if (value == null) return null;
+      if (value is int) {
+        return DateTime.fromMillisecondsSinceEpoch(value);
+      }
+      return DateTime.parse(value);
+      // ignore: avoid_catches_without_on_clauses
+    } catch (e) {
+      return null;
+    }
   }
 
   /// Parses a [String] into a [DateTime].
