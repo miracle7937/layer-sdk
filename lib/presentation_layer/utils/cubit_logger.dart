0a4de84903e39ea64a408be643f8dfa2a2076f1d
saulo andrioli <saulo.andrioli@wearelayer.com>
2023-04-06 11:03:44 -0300

    Adds CubitLogger class

---
+++ lib/presentation_layer/utils/cubit_logger.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/foundation.dart';
+import 'package:logging/logging.dart';
+
+import '../../data_layer/network.dart';
+
+/// This class is used to log cubit method exceptions
+mixin CubitLogger {
+  /// Logs an exception
+  /// so it can be stored in a [Logger] file by [FileLogger] class
+  static void logException(Exception e, StackTrace st) {
+    /// TODO - In the future, we need to find a better string to use to identify this in the logs.
+    /// For now it is staying as `SystemStateError`
+    final _log = Logger('SystemStateError');
+    final frame = StackFrame.fromStackTrace(st).first;
+    final exceptionString =
+        e is NetException ? (e.message ?? '') : e.toString();
+
+    /// <CubitName> - <MethodName>: <Exception>
+    _log.log(
+      Level.SEVERE,
+      "${frame.className} - ${frame.method} exception: $exceptionString",
+    );
+  }
+}
